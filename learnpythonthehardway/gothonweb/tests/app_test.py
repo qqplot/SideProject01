0a27f3ca56aad54c9518592fc4991064933d739d
kyu <kyubyung91@gmail.com>
2019-12-15 19:37:21 +0900

    pytest 변경
    세션 테스트 추가

--- learnpythonthehardway/gothonweb/tests/app_test.py
+++ learnpythonthehardway/gothonweb/tests/app_test.py
@@ -1,18 +1,21 @@
 from app import app
+from flask import session
 
 app.config['TESTING'] = True
-web = app.test_client()
 
 def test_index():
-    rv = web.get('/', follow_redirects=True)
-    assert rv.status_code == 404
+    with app.test_client() as web:
+        rv = web.get('/', follow_redirects=True)
+        assert rv.status_code == 200
 
-    rv = web.get('/game', follow_redirects=True)
-    assert rv.status_code == 200
-    assert rv.session.get('방_이름') == '중앙_복도'
-    assert "이 폼을 채우세요" in rv.data.decode()
+        rv = web.get('/game', follow_redirects=True)
+        assert rv.status_code == 200
 
-    data = {'name': '제드', 'greet': 'Hello'}
-    rv = web.post('/hello', follow_redirects=True, data=data)
-    assert "제드" in rv.data.decode()
-    assert b"Hello" in rv.data
+        with web.session_transaction() as sess:
+            assert sess['방_이름'] == '중앙_복도'
+#    assert "이 폼을 채우세요" in rv.data.decode()
+
+#    data = {'name': '제드', 'greet': 'Hello'}
+#    rv = web.post('/hello', follow_redirects=True, data=data)
+#    assert "제드" in rv.data.decode()
+#    assert b"Hello" in rv.data
